257153805c20846fd1b6679219a10449716d2116
Wasley <82953511+wasleysantos@users.noreply.github.com>
2024-12-03 11:24:38 -0300

    Update Helloword.c

--- Helloword.c
+++ Helloword.c
@@ -4,6 +4,7 @@
 int main(){
     printf("Hello, world! \n");
     printf("Seja bem-vindo! \n");
+    printf("Seja bem-vindo! \n");
     system("pause");
     //primeiro programa em C, Que DEUS abençõe nessa jornada
 }
